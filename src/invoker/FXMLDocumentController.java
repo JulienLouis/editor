ab022e19ffc304f17e8610f90fb5ec189bca694b
JulienLouis <julien.louis25@yahoo.fr>
2016-11-17 16:54:42 +0100

    before import to inteliJ

---
+++ src/invoker/FXMLDocumentController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package invoker;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.*;
+import javafx.fxml.*;
+import javafx.scene.control.*;
+
+/**
+ *
+ * @author 17012776
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
