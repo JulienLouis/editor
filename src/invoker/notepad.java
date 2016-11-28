cc7978e0b9c1d4ef66793c8c5ce2f88c0ff90cad
JulienLouis <julien.louis25@yahoo.fr>
2016-11-28 11:20:49 +0100

    problem pull

--- src/invoker/notepad.java
+++ src/invoker/notepad.java
@@ -1,210 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 package invoker;
-import javax.swing.*;
-import javax.swing.event.CaretEvent;
-import javax.swing.event.CaretListener;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.Document;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import command.command;
+import javafx.event.*;
+import javafx.fxml.*;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.*;
+import javafx.application.*;
 import receiver.java.model.MoteurEditionImplementation;
 import command.*;
-import client.Editeur;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ *
+ * @author 17012776
+ */
+public class Notepad extends Application {
+
+    client client = new client();
+
+    MoteurEditionImplementation mei = new MoteurEditionImplementation();
+    public command collerC;
+
+    @Override
+   // public void start(Stage stage) throws Exception {
+    public void start(Stage stage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root,800,600);
+        
+        stage.setScene(scene);
+        stage.show();
+
+        collerC = new coller(mei);
+
+    }
 
 
-public class notepad extends JFrame implements Observer{   
-	
-	private Editeur e;
-	
-	//command
-	private command copierC;
-	private command couperC;
-	private command collerC;
-	private command selectionC;
-	private command insererC;
-	private command supprimerC;
-	
-	//swing interface
-	private JButton copier;
-	private JButton couper;
-	private JButton coller;
-	private JButton selection;
-	private JTextArea textArea;
-	
-	//Int debut fin de la selection
-	int[] debutfin = new int[2];
-	
-  public notepad(Editeur editeur) {
-	  
-	  	copierC = new copier(editeur);
-		couperC = new couper(editeur);
-		collerC = new coller(editeur);
-		selectionC = new Selectionner(editeur, this);
-		supprimerC = new supprimer_texte(editeur);
-	  
-	  	e = editeur;
-	  
-	  	this.setSize(650, 650);
-		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setTitle("Editeur de texte");
+    @FXML
+    private Label label;
+    @FXML
+    private Button button1;
+    @FXML
+    private Button button2;
+    @FXML
+    private Button button3;
+    @FXML
+    private Button button4;
+    @FXML
+    private Button button5;
+    @FXML
+    private TextArea textArea;
 
-		// Creation du JPanel principal
-		JPanel mainPanel = new JPanel();
-		// Creation du BorderLayout
-		mainPanel.setLayout(new BorderLayout());
-		// Ajout du JPanel a la JFrame
-		this.add(mainPanel);
+    @FXML
+    public void HandleInsererButtonAction(ActionEvent event){
+        button4.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                client.list.get("inserer_texte");
+            }
+        });
+    }
+    @FXML
+    public void HandleCollerButtonAction(ActionEvent event){
+        button2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                collerC.execute();
+                textArea.setText(mei.buf.zone_texte.toString());
+            }
+        });
+    }
+    @FXML
+    public void HandleCopierButtonAction(ActionEvent event){
+        button3.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                client.list.get("copier");
+            }
+        });
+    }
+    @FXML
+    public void HandleCouperButtonAction(ActionEvent event){
+        button1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                client.list.get("couper");
+            }
+        });
+    }
 
-		// Initialisation des boutons
-		copier = new JButton("Copier");
-		couper = new JButton("Couper");
-		coller = new JButton("Coller");
+    @FXML
+    public void HandleSupprimerButtonAction(ActionEvent event){
+        button5.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                client.list.get("supprimer");
+            }
+        });
+    }
 
-		// Desactivation boutons
-		coller.setEnabled(false);
-		
-		JPanel upperPanel = new JPanel();
-		upperPanel.add(copier);
-		upperPanel.add(couper);
-		upperPanel.add(coller);
-		mainPanel.add(upperPanel, BorderLayout.NORTH);
 
-		// Creation du JTextArea
-		textArea = new JTextArea();
-		Font f = new Font("Times New Roman", Font.PLAIN, 18);
-		textArea.setFont(f);
-		mainPanel.add(textArea, BorderLayout.CENTER);
-	  	textArea.getDocument().addDocumentListener(new MyDocumentListener());
-	    textArea.getDocument().putProperty("name", "Text Area");
-		
-		//ajout des listener
-		commandButton cb = new commandButton();
-		commandSelection cS = new commandSelection();
-		textArea.addCaretListener(cS);
-		copier.addActionListener(cb);
-		coller.addActionListener(cb);
-		couper.addActionListener(cb);
 
-		this.setVisible(true);
-	  
-  }
-  
-  	private class MyDocumentListener implements DocumentListener {
-  		
-	    final String newline = "\n";
-	
-	    public void insertUpdate(DocumentEvent e) {
-	        updateLog(e, "inserted into");
-	    }
-	    public void removeUpdate(DocumentEvent e) {
-	        updateLog(e, "removed from");
-	    }
-	    public void changedUpdate(DocumentEvent e) {
-	        //Plain text components don't fire these events.
-	    }
-	
-	    public void updateLog(DocumentEvent e, String action) {
-	        Document doc = (Document)e.getDocument();
-	        int changeLength = e.getLength();
-	       
-		    //le contenue de textArea
-	          System.out.println(textArea.getText());	          
-	          //Les informations sur le contenue de textArea
-	          /*  
-	          System.out.println(
-	              changeLength + " character"
-	            + ((changeLength == 1) ? " " : "s ")
-	            + action + " " + doc.getProperty("name") + "."
-	            + newline
-	            + "  Text length = " + doc.getLength() + newline);
-	         */
-		            
-      }
-  }
-	
-	//This listens for and reports caret movements.
-  	private class CaretListenerLabel extends JTextArea implements CaretListener {	 
-  		//Might not be invoked from the event dispatch thread.
-  		public void caretUpdate(CaretEvent e) {
-  			displaySelectionInfo(e.getDot(), e.getMark());
-  		}
 
-	     //This method can be invoked from any thread.  It 
-	     //invokes the setText and modelToView methods, which 
-	     //must run on the event dispatch thread. We use
-	     //invokeLater to schedule the code for execution
-	     //on the event dispatch thread.
-	     public void displaySelectionInfo(final int dot,
-	                                         final int mark) {
-	         SwingUtilities.invokeLater(new Runnable() {
-	        	 final String newline = "\n";
-	        	 public void run() {
-	                 if (dot == mark) {  // no selection
-	                     try {
-	                         Rectangle caretCoords =  textArea.modelToView(dot);   
-	                         //Convert it to view coordinates.
-	                         
-	                         System.out.println("caret: text position: " + dot
-	                                 + ", view location = ["
-	                                 + caretCoords.x + ", "
-	                                 + caretCoords.y + "]"
-	                                 + newline);
-	                     } catch (BadLocationException ble) {
-	                    	 System.out.println("caret: text position: " + dot + newline);
-	                     }
-	                 } else if (dot < mark) {
-	                	 System.out.println("selection from: " + dot
-	                             + " to " + mark + newline);
-	                 } else {
-	                	 System.out.println("selection from: " + mark
-	                             + " to " + dot + newline);
-	                 }
-	             }
-	         });
-	     }
-  	}
-  	
-  	public int[] getSelectionDebutFin(){
-  		return debutfin;
-  	}
-	
-  	//Permet de récupérer les entier de la selection et de lancer la command selectionC
-  	private class commandSelection implements CaretListener {
-		public void caretUpdate(CaretEvent e) {
-			int newDebut = Math.min(e.getDot(), e.getMark());
-			int newFin = Math.max(e.getDot(), e.getMark());
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-			if (newDebut != debutfin[0] || newFin != debutfin[1]) {
-				debutfin[0] = newDebut;
-				debutfin[1] = newFin;
-				selectionC.execute();
-				}
-			}
-	}
-	
-  	//Lance les command collerC copierC et couperC
-	private class commandButton implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-				if (e.getSource() == copier) {
-					coller.setEnabled(true);
-					copierC.execute();
-				} else if (e.getSource() == couper) {
-					couperC.execute();
-					coller.setEnabled(true);
-				} else if (e.getSource() == coller)
-					collerC.execute();
-			}
-	  }
-	  
-	    
-		  
-	  public void update(String contenu) {
-			textArea.setText(contenu);
-	  }
-	  
-	}
+}
