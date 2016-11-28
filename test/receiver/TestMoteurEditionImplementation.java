3a527aee0be4ea31b8ba65d56aa793519c6c9f57
JulienLouis <julien.louis25@yahoo.fr>
2016-11-28 20:17:12 +0100

    Update TestMoteurEditionImplementation.java

--- test/receiver/TestMoteurEditionImplementation.java
+++ test/receiver/TestMoteurEditionImplementation.java
@@ -1,51 +1,40 @@
-package receiver;
+package editor;
 import java.util.Set;
 import java.util.HashSet;
-import receiver.java.model.*;
+import receiver.*;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class TestMoteurEditionImplementation {
 	
-	/*
-	fail(String) 
-	assertTrue(true) 
-	assertsEquals([String message], expected, actual) 
-	assertsEquals([String message], expected, actual, tolerance)
-	assertNull([message], object)
-	assertNotNull([message], object) 
-	assertSame([String], expected, actual) 
-	assertNotSame([String], expected, actual)
-	assertTrue([message], boolean condition) 
-	*/
 
 	@Test
 	public void test() {
-		MoteurEditionImplementation mei = new MoteurEditionImplementation();
-		mei.buf.zone_texte = new StringBuffer("coincoin");
-		mei.pp.contenu_presse_papier = "";
-		mei.selectionner(8, 8);
-		mei.inserer_texte(" toucan");//cas à la fin de la chaine
-		assertEquals("L'insertion en fin de chaine n'est pas la bonne ou pas au bon endroit", mei.buf.zone_texte.toString().substring(8, mei.buf.zone_texte.toString().length()), " toucan");
-		mei.selectionner(8, 8);
-		mei.inserer_texte(" dindon");//cas au milieu de la chaine
-		assertEquals("L'insertion en milieu de chaine n'est pas la bonne ou pas au bon endroit", mei.buf.zone_texte.toString().substring(8, 15), " dindon");
-		mei.selectionner(0, 8);
-		mei.inserer_texte("canard");//cas au début de la chaine et en remplaçant du texte
-		assertEquals("L'insertion en fin de chaine n'est pas la bonne ou pas au bon endroit", mei.buf.zone_texte.toString().substring(0, 8), "canard d");
-		mei.selectionner(6, 13);
-		mei.copier();	//Seul cas de la copie
-		assertEquals("La copie du texte n'est pas dans le presse papier", mei.pp.contenu_presse_papier, " dindon");
-		mei.selectionner(mei.buf.zone_texte.toString().length(), mei.buf.zone_texte.toString().length());
-		mei.coller();	//colle à la fin de la chaine
-		assertEquals("Le collage en fin de chaine n'est pas le bon ou pas au bon endroit", mei.buf.zone_texte.toString().substring(20, mei.buf.zone_texte.toString().length()), " dindon");
-		mei.selectionner(13, mei.buf.zone_texte.toString().length());
-		mei.couper();
-		assertEquals("Le contenu du presse papier ne correspond pas à la coupe", mei.pp.contenu_presse_papier, " toucan dindon");
-		assertEquals("Le texte n'a pas été coupé", mei.buf.zone_texte.toString(), "canard dindon");
-		mei.selectionner(6, 13);
-		mei.coller();	//Collage en remplaçant une partie du texte
-		assertEquals("Le texte coller en remplaçant une autre chaine fonctionne",mei.buf.zone_texte.toString(), "canard toucan dindon");	
+		MoteurEditionImpl moteur = new MoteurEditionImpl();
+		moteur.getBuffer().setContenu("coincoin");
+		moteur.getPressePapier().setContenu("");
+		moteur.selectionner(8, 8);
+		moteur.saisir(" toucan");//cas Ã  la fin de la chaine
+		assertEquals("L'insertion en fin de chaine n'est pas la bonne ou pas au bon endroit", moteur.getBuffer().getContenu().substring(8, moteur.getBuffer().getContenu().length()), " toucan");
+		moteur.selectionner(8, 8);
+		moteur.saisir(" dindon");//cas au milieu de la chaine
+		assertEquals("L'insertion en milieu de chaine n'est pas la bonne ou pas au bon endroit", moteur.getBuffer().getContenu().substring(8, 15), " dindon");
+		moteur.selectionner(0, 8);
+		moteur.saisir("canard");//cas au dÃ©but de la chaine et en remplaÃ§ant du texte
+		assertEquals("L'insertion en fin de chaine n'est pas la bonne ou pas au bon endroit", moteur.getBuffer().getContenu().substring(0, 8), "canard d");
+		moteur.selectionner(6, 13);
+		moteur.copier();	//Seul cas de la copie
+		assertEquals("La copie du texte n'est pas dans le presse papier", moteur.getPressePapier().getContenu(), " dindon");
+		moteur.selectionner(moteur.getBuffer().getContenu().length(), moteur.getBuffer().getContenu().length());
+		moteur.coller();	//colle Ã  la fin de la chaine
+		assertEquals("Le collage en fin de chaine n'est pas le bon ou pas au bon endroit", moteur.getBuffer().getContenu().substring(20, moteur.getBuffer().getContenu().length()), " dindon");
+		moteur.selectionner(13, moteur.getBuffer().getContenu().length());
+		moteur.couper();
+		assertEquals("Le contenu du presse papier ne correspond pas Ã  la coupe", moteur.getPressePapier().getContenu(), " toucan dindon");
+		assertEquals("Le texte n'a pas Ã©tÃ© coupÃ©", moteur.getBuffer().getContenu().toString(), "canard dindon");
+		moteur.selectionner(6, 13);
+		moteur.coller();	//Collage en remplaÃ§ant une partie du texte
+		assertEquals("Le texte coller en remplaÃ§ant une autre chaine fonctionne",moteur.getBuffer().getContenu().toString(), "canard toucan dindon");	
 	}	
 }
